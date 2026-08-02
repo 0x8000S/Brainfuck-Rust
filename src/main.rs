3512f9e27881187abe618e9d79ff824af764281f
0x8000S <BlueRectS@outlook.com>
2026-08-02 20:29:29 +0800

    更新版本号至 1.2.0，增强解释器功能

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,9 @@ enum Token {
     LEFTPAR,
     RIGHTPAR,
     FUNC(String, Vec<Token>),
-    CALLFUNC(String)
+    CALLFUNC(String),
+    IMPORT(String),
+    ENDIMPORT
 }
 impl Token {
     fn exec(&self, map: &mut EnvVec) {
@@ -155,7 +157,13 @@ impl Token {
                 for t in map.get_func(name) {
                     t.exec(map);
                 }
-            }
+            },
+            Token::IMPORT(mo) => {
+                for t in parse(read_code_from_file(mo).as_str()).0 {
+                    t.exec(map);
+                }
+            },
+            Token::ENDIMPORT => ()
         }
     }
 }
@@ -188,20 +196,46 @@ impl CommandContent {
     }
 }
 
-fn get_len_token(toks: &Vec<Token>) -> usize {
-    let mut len = 0;
-    for tok in toks.iter() {
-        len += match tok {
-            Token::LEFTW(l) => {
-                get_len_token(l) + 1
-            },
-            _ => 1
-        }
+fn is_num(num: &str) -> bool {
+    match num.parse::<u64>() {
+        Ok(v) => true,
+        Err(_) => false
     }
-    len
 }
 
-fn parse(command: &str) -> Vec<Token> {
+fn parse_to_token(command: &str) -> Token {
+    match command {
+        "<" => Token::LEFT,
+        ">" => Token::RIGHT,
+        "+" => Token::PLUS,
+        "-" => Token::SUB,
+        "." => Token::PRT,
+        "," => Token::READ,
+        "[" => Token::LEFTW(vec![]),
+        "]" => Token::RIGHTW,
+        "=" => Token::SZ,
+        "~" => Token::BITNOT,
+        "{" => Token::LM,
+        "}" => Token::RM,
+        "!" => Token::INP,
+        ":" => Token::PUSH,
+        ";" => Token::POP,
+        "(" => Token::LEFTPAR,
+        ")" => Token::RIGHTPAR,
+        "`" => Token::FUNC(String::new(), vec![]),
+        "@" => Token::IMPORT(String::new()),
+        "|" => Token::ENDIMPORT,
+        _ => {
+            if is_num(&command) {
+                Token::NUM(0)
+            }else {
+                Token::CALLFUNC(String::new())
+            }
+        }
+    }
+}
+
+fn parse(command: &str) -> (Vec<Token>, usize) {
     let mut tokens: Vec<Token> = Vec::new();
     let commands: Vec<String> = command.chars()
         .map(|x| x.to_string())
@@ -234,15 +268,18 @@ fn parse(command: &str) -> Vec<Token> {
             "[" => {
                 let mut p_command = Vec::new();
                 // dbg!(&iter);
+                dbg!(&command_iter);
                 // println!("ZSX{}", iter.to_string());
                 let curr = parse(command_iter.to_string().as_str()).clone();
-                command_iter.index += get_len_token(&curr);
+                // command_iter.index += get_len_token(&curr.0);
+                command_iter.index += curr.1;
                 // dbg!(&curr);
-                if !curr.contains(&Token::RIGHTW) {
+                if !curr.0.contains(&Token::RIGHTW) {
+                    dbg!(&curr);
                     eprintln!("[]未闭合!");
                     exit(-1);
                 }
-                for i in curr {
+                for i in curr.0 {
                     p_command.push(i)
                 }
                 // dbg!(&iter);
@@ -253,18 +290,8 @@ fn parse(command: &str) -> Vec<Token> {
             "]" => {
                 tokens.push(Token::RIGHTW);
                 // dbg!(&tokens);
-                return tokens
+                return (tokens, command_iter.index)
             },
-            "0" => Token::NUM(0),
-            "1" => Token::NUM(1),
-            "2" => Token::NUM(2),
-            "3" => Token::NUM(3),
-            "4" => Token::NUM(4),
-            "5" => Token::NUM(5),
-            "6" => Token::NUM(6),
-            "7" => Token::NUM(7),
-            "8" => Token::NUM(8),
-            "9" => Token::NUM(9),
             "=" => Token::SZ,
             "~" => Token::BITNOT,
             "{" => Token::LM,
@@ -275,23 +302,24 @@ fn parse(command: &str) -> Vec<Token> {
             "(" => Token::LEFTPAR,
             ")" => {
                 tokens.push(Token::RIGHTPAR);
-                return tokens;
+                return (tokens, command_iter.index);
             }
-            "'" => {
+            "`" => {
                 let mut func_name = String::new();
                 let mut func_body = Vec::new();
                 loop {
                     match command_iter.next() {
                         Some(val) => {
-                            if parse(val.as_str())[0] == Token::LEFTPAR {
+                            if parse_to_token(val.as_str()) == Token::LEFTPAR {
                                 let ret = parse(command_iter.to_string().as_str());
                                 // dbg!(&ret);
-                                command_iter.index += get_len_token(&ret);
-                                if !ret.contains(&Token::RIGHTPAR) {
+                                // command_iter.index += get_len_token(&ret.0);
+                                command_iter.index += ret.1;
+                                if !ret.0.contains(&Token::RIGHTPAR) {
                                     eprintln!("未找到闭合的()!");
                                     exit(-3);
                                 }
-                                for t in ret {
+                                for t in ret.0 {
                                     func_body.push(t);
                                 }
                                 break;
@@ -303,38 +331,108 @@ fn parse(command: &str) -> Vec<Token> {
                     }
                 }
                 Token::FUNC(func_name, func_body)
-            }
-            _ => {
-                let mut call_func_name = String::new();
-                call_func_name.push_str(t.as_str());
+            },
+            "@" => {
+                let mut file = String::new();
+                let mut close = false;
                 loop {
                     match command_iter.peek() {
-                        Some(val) => match parse(val.as_str())[0] {
-                            Token::CALLFUNC(_) => (),
-                            _ => break
+                        Some(val) => match parse_to_token(val.as_str()) {
+                            Token::ENDIMPORT => {
+                                close = true;
+                                break
+                            },
+                            _ => ()
                         },
                         None => break
                     }
                     match command_iter.next() {
                         Some(val) => {
-                            // match parse(val.as_str())[0] {
-                            //     Token::CALLFUNC(_) => (),
-                            //     _ => {
-                            //         command_iter.index -= 1;
-                            //         continue
-                            //     }
-                            // }
-                            call_func_name.push_str(val.as_str());
+                            file.push_str(val.as_str());
                         },
                         None => break
                     }
                 }
-                Token::CALLFUNC(call_func_name)
+                if !close {
+                    eprintln!("@未闭合|!");
+                    exit(-5);
+                }
+                Token::IMPORT(file)
+            },
+            "|" => Token::ENDIMPORT,
+            _ => {
+                if is_num(&t) {
+                    let mut num = String::new();
+                    num.push_str(t.as_str());
+                    loop {
+                        match command_iter.peek() {
+                            Some(val) => match parse_to_token(val.as_str()) {
+                                Token::NUM(_) => (),
+                                _ => break
+                            },
+                            None => break
+                        }
+                        match command_iter.next() {
+                            Some(val) => {
+                                num.push_str(val.as_str());
+                            },
+                            None => break
+                        }
+                    }
+                    // dbg!(&command_iter);
+                    Token::NUM((num.parse::<u64>().unwrap() % 256) as u8)
+                }else {
+                    let mut call_func_name = String::new();
+                    call_func_name.push_str(t.as_str());
+                    loop {
+                        match command_iter.peek() {
+                            Some(val) => match parse_to_token(val.as_str()) {
+                                Token::CALLFUNC(_) => (),
+                                _ => break
+                            },
+                            None => break
+                        }
+                        match command_iter.next() {
+                            Some(val) => {
+                                // match parse(val.as_str())[0] {
+                                //     Token::CALLFUNC(_) => (),
+                                //     _ => {
+                                //         command_iter.index -= 1;
+                                //         continue
+                                //     }
+                                // }
+                                call_func_name.push_str(val.as_str());
+                            },
+                            None => break
+                        }
+                    }
+                    Token::CALLFUNC(call_func_name)
+                }
             }
         };
         tokens.push(curr_com);
     }
-    tokens
+    (tokens, command_iter.index)
+}
+
+fn read_code_from_file(file: &String) -> String {
+    match fs::read_to_string(file) {
+        Ok(val) => {
+            let mut content = String::new();
+            for l in val.lines() {
+                let lw = match l.find("#") {
+                        Some(v) => l[..v].to_string(),
+                        None => l.to_string()
+                    };
+                content.push_str(lw.trim())
+                }
+            content
+        },
+        Err(e) => {
+            eprintln!("错误: {e}");
+            exit(-2);
+        }
+    }
 }
 
 fn main() {
@@ -363,24 +461,17 @@ fn main() {
         }
     } else {
         let file = &args[1];
-        match fs::read_to_string(file) {
-            Ok(val) => {
-                let mut env = EnvVec{
-                    data: vec![0],
-                    index: 0,
-                    stack: vec![],
-                    func: HashMap::new()
-                };
-                run(val.as_str(), &mut env);
-            },
-            Err(e) => {
-                eprintln!("错误: {e}");
-                exit(-2);
-            }
-        }
+        let mut env = EnvVec{
+            data: vec![0],
+            index: 0,
+            stack: vec![],
+            func: HashMap::new()
+        };
+        let content = read_code_from_file(file);
+        run(content.as_str(), &mut env);
     }
-    // let test = "'b(--)'a(+++b)a!";
-    // // 输出的结果是 1
+    // let test = "`add(67)add.";
+    // // 输出了A-Z
     // let mut env = EnvVec {
     //     data: vec![0],
     //     index: 0,
@@ -391,9 +482,10 @@ fn main() {
 }
 
 fn run(test: &str, env: &mut EnvVec) {
+    // println!("{test}");
     let ret = parse(test);
-    // dbg!(&ret);
-    for tok in ret {
+    // dbg!(&ret.0);
+    for tok in ret.0 {
         tok.exec(env);
         // dbg!(&env);
     }
